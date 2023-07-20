f94759a1cf53caa0d496f0937b58874013bca521
Carla Reis <72628725+Carlabreis@users.noreply.github.com>
2023-07-20 22:16:14 +0000

    01_05 exercise

--- start/01_05_private_object.js
+++ start/01_05_private_object.js
@@ -1,1 +1,14 @@
 // Write your code here
+const username = Symbol("username");
+const password = Symbol("password");
+
+
+const user = {
+    [username]: 'carla',
+    [password]: '1234',
+    age: 27
+}
+
+
+console.log(user.username);
+console.log(user.password);
