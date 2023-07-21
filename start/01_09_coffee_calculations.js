32f66a35c55e4ac7dbcba673b97ad1c76913d6e9
Carla Reis <72628725+Carlabreis@users.noreply.github.com>
2023-07-21 17:16:41 +0000

    01_09 array.reduce exercise

--- start/01_09_coffee_calculations.js
+++ start/01_09_coffee_calculations.js
@@ -1,1 +1,8 @@
 // Write your code here
+function totalBill(arr){
+  let total = arr.reduce((acc, cur) => (acc += cur)
+  );
+  return `The total bill is ${total*1.25}`;
+}
+
+console.log(totalBill([1,2,3]));
