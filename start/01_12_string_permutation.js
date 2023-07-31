57b2e72032b12c8e2bc029f173b0b28aad730814
Carla Reis <72628725+Carlabreis@users.noreply.github.com>
2023-07-31 15:57:31 +0000

    01_12 recursion exercise

--- start/01_12_string_permutation.js
+++ start/01_12_string_permutation.js
@@ -1,1 +1,23 @@
 // Write your code here
+
+// function scrabble(letters) {
+//   let total;
+//   for (let i = 1; i <= letters.length; i++){
+//     if (total === undefined){
+//       total = i;
+//       continue;
+//     }
+//     total *= i;
+//   }
+//   return total;
+// }
+
+// RECURSION function
+function scrabble(letters) {
+  if (letters.length === 1) {
+    return 1;
+  }
+  return letters.length * scrabble(letters.slice(1));
+}
+
+console.log(scrabble("hello"));
