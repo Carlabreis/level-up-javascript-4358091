026fda97f1ea8ae675d188cd0af30b75b463881a
Carla Reis <72628725+Carlabreis@users.noreply.github.com>
2023-07-31 16:24:42 +0000

    01_14 string manipulation exercise

--- start/01_14_urlify.js
+++ start/01_14_urlify.js
@@ -1,1 +1,7 @@
 // Write your code here
+function turnToUrlName(title) {
+  //punctuation removed, case lowered, spaces replaced with hyphens
+  return title.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').split(' ').join('-');
+}
+
+console.log(turnToUrlName("My blog title!"));
